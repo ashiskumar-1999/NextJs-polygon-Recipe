2469ad0f331c2e9ad45e31c13a2dfd041930af6b
Ashis Kumar <ashishsahoo389@gmail.com>
2022-03-30 23:40:33 +0530

    A demo smart contract has been written, executed in polygon network and interacted with the UI.

---
+++ components/Comment.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+import { Text, Heading, HStack, Stack } from "@chakra-ui/react";
+import TimeAgo from "react-timeago";
+import Avatar from "@davatar/react";
+import Username from "./UserName";
+import { Comment } from "../hooks/useCommentsContract";
+
+interface CommentProps {
+  comment: Comment;
+}
+
+const Comment: React.FunctionComponent<CommentProps> = ({ comment }) => {
+  return (
+    <HStack spacing={3} alignItems="start">
+      <Avatar size={48} address={comment.creator_address} />
+      <Stack spacing={1} flex={1} bg="whiteAlpha.100" rounded="2xl" p={3}>
+        <Username address={comment.creator_address}/>
+        <Text color="whiteAlpha.800" fontSize="lg">
+          {comment.message}
+        </Text>
+        <Text color="whiteAlpha.500" fontSize="md">
+          <TimeAgo date={comment.created_at.toNumber() * 1000} />
+        </Text>
+      </Stack>
+    </HStack>
+  );
+};
+
+export default Comment;
